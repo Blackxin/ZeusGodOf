05d1a9f1bdd263ee022ba22fa9c15882752fa966
Zeus <99333164+zeusop5@users.noreply.github.com>
2022-09-26 22:30:43 +0530

    Update __init__.py

--- MKXSpam/__init__.py
+++ MKXSpam/__init__.py
@@ -32,7 +32,7 @@ BOT_TOKEN8 = config("BOT_TOKEN8", default=None)
 BOT_TOKEN9 = config("BOT_TOKEN9", default=None)
 BOT_TOKEN10 = config("BOT_TOKEN10", default=None)
 SUDO_USERS = list(map(int, getenv("SUDO_USER").split()))
-
+OWNER_NAME = os.environ.get("OWNER_NAME", None)
 OWNER_ID = int(os.environ.get("OWNER_ID", None))
 
 # Don't Mess with Codes !! 
